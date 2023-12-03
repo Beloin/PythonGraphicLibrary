8da46ba379de5dabe3a46257a3ecdd462361076c
Beloin Sena <beloin.rodrigues@gmail.com>
2023-12-03 18:03:06 -0300

    Simple math in utils.py

---
+++ camera_persp/camera.py
@@ -0,0 +1,6 @@
+# Based o n a -20:20 World
+from types_3d import Point3D
+
+
+def get_eye(at: Point3D) -> Point3D:
+    pass
---
+++ util.py
@@ -0,0 +1,23 @@
+import math
+
+from types_3d import Point3D
+
+
+def project(p: Point3D, n: Point3D) -> Point3D:
+    a = p[0] * n[0] + p[1] * n[1] + p[2] * n[2]
+    b = n[0] * n[0] + n[1] * n[1] + n[2] * n[2]
+    res = a / b
+    return n[0] * res, n[1] * res, n[0] * res
+
+
+def normalize(p: Point3D) -> Point3D:
+    res = p[0] * p[0] + p[1] * p[1] + p[2] * p[2]
+    res = math.sqrt(res)
+    return p[0] / res, p[1] / res, p[0] / res
+
+
+def vetorial_product(u: Point3D, n: Point3D) -> Point3D:
+    i = u[1] * n[2] - u[2] * n[1]
+    j = u[2] * n[0] - u[0] * n[2]
+    k = u[0] * n[1] - u[1] * n[0]
+    return i, j, k
