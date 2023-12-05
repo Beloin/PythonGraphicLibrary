a21773a8b3a85f3ac7c4ac0e96b034ec2b05423d
Beloin Sena <beloin.rodrigues@gmail.com>
2023-12-04 22:19:18 -0300

    Fixed division by zero problem

--- camera_persp/camera.py
+++ camera_persp/camera.py
@@ -8,21 +8,25 @@ import numpy as np
 
 
 def get_camera(at: Point3D, eye: Point3D) -> Coord3D:
+    assert at != eye, "`at` should be different than `eye`"
     at_eye = (at[0] - eye[0], at[1] - eye[1], at[2] - eye[2])
     n = math_utils.normalize(at_eye)
-    aux = (n[0], random.randint(0, 100) / 100, n[2])  # TODO: Use random aux?
+    aux = (n[0], random.randint(0, 100) / 100, n[2])
+    if aux[0] == 0 and aux[2] == 0:
+        aux = (aux[0], aux[1], random.randint(0, 100) / 100)
+
     aux_p = math_utils.project(aux, n)
     u = math_utils.minus(aux, aux_p)
     u = math_utils.normalize(u)
     v = math_utils.vetorial_product(u, n)
 
-    return u, v, n
+    return u, v, n  # TODO: Check which is better...
 
 
 def point_to_camera(p: Point3D, camera: Coord3D, eye: Point3D) -> Point3D:
     # p' = RT * p
     #  | V1 V2 V3 0 |       | 1  0  0 E1 |
-    #  | U1 U2 U3 0 |   *   | 0  1  0 E2 | // TODO: Change
+    #  | U1 U2 U3 0 |   *   | 0  1  0 E2 |
     #  | N1 N2 N3 0 |       | 0  0  1 E3 |
     #  | 0  0  0  1 |       | 0  0  0  1 |
     a1 = -eye[0] * camera[0][0] - eye[1] * camera[0][1] - eye[2] * camera[0][2]
@@ -35,16 +39,6 @@ def point_to_camera(p: Point3D, camera: Coord3D, eye: Point3D) -> Point3D:
     return px, py, pz
 
 
-def point_to_camera_np(p: Point3D, camera: Coord3D, eye: Point3D):
-    # p' = RT * p
-    #  | V1 V2 V3 0 |       | 1  0  0 E1 |
-    #  | U1 U2 U3 0 |   *   | 0  1  0 E2 |
-    #  | N1 N2 N3 0 |       | 0  0  1 E3 |
-    #  | 0  0  0  1 |       | 0  0  0  1 |
-
-    t = np.array([[], [], []])
-
-
 def edges_to_camera(edges: Vec3DList, camera: Coord3D, eye: Point3D):
     new_edges = Vec3DList(sep=edges.sep())
     for edge in edges:
@@ -53,3 +47,10 @@ def edges_to_camera(edges: Vec3DList, camera: Coord3D, eye: Point3D):
         new_edges.append((p1, p2))
 
     return new_edges
+
+
+if __name__ == '__main__':
+    at = (0, 0, 0)
+    eye = (0, 0, 1)
+    camera = get_camera(at, eye)
+    print(camera)
