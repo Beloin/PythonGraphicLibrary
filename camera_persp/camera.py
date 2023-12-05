73c3e5a0e82710772553ca41984eafafa4f12d64
Beloin Sena <beloin.rodrigues@gmail.com>
2023-12-04 21:44:23 -0300

    Looks like we made camera trans

--- camera_persp/camera.py
+++ camera_persp/camera.py
@@ -1,32 +1,55 @@
 # Based o n a -20:20 World
+import random
+
 from types_3d import Point3D, Coord3D
 import math_utils
+from wireframe import Vec3DList
+import numpy as np
 
 
 def get_camera(at: Point3D, eye: Point3D) -> Coord3D:
     at_eye = (at[0] - eye[0], at[1] - eye[1], at[2] - eye[2])
     n = math_utils.normalize(at_eye)
-    aux = (n[0], n[1], 0)  # TODO: Use random aux?
+    aux = (n[0], random.randint(0, 100) / 100, n[2])  # TODO: Use random aux?
     aux_p = math_utils.project(aux, n)
     u = math_utils.minus(aux, aux_p)
     u = math_utils.normalize(u)
     v = math_utils.vetorial_product(u, n)
 
-    return v, u, n
+    return u, v, n
 
 
 def point_to_camera(p: Point3D, camera: Coord3D, eye: Point3D) -> Point3D:
     # p' = RT * p
     #  | V1 V2 V3 0 |       | 1  0  0 E1 |
-    #  | U1 U2 U3 0 |   *   | 0  1  0 E2 |
+    #  | U1 U2 U3 0 |   *   | 0  1  0 E2 | // TODO: Change
     #  | N1 N2 N3 0 |       | 0  0  1 E3 |
     #  | 0  0  0  1 |       | 0  0  0  1 |
     a1 = -eye[0] * camera[0][0] - eye[1] * camera[0][1] - eye[2] * camera[0][2]
     a2 = -eye[0] * camera[1][0] - eye[1] * camera[1][1] - eye[2] * camera[1][2]
-    a3 = -eye[0] * camera[1][0] - eye[1] * camera[2][1] - eye[2] * camera[2][2]
+    a3 = -eye[0] * camera[2][0] - eye[1] * camera[2][1] - eye[2] * camera[2][2]
     px = camera[0][0] * p[0] + camera[0][1] * p[1] + camera[0][2] * p[2] + a1
     py = camera[1][0] * p[0] + camera[1][1] * p[1] + camera[1][2] * p[2] + a2
     pz = camera[2][0] * p[0] + camera[2][1] * p[1] + camera[2][2] * p[2] + a3
 
     return px, py, pz
 
+
+def point_to_camera_np(p: Point3D, camera: Coord3D, eye: Point3D):
+    # p' = RT * p
+    #  | V1 V2 V3 0 |       | 1  0  0 E1 |
+    #  | U1 U2 U3 0 |   *   | 0  1  0 E2 |
+    #  | N1 N2 N3 0 |       | 0  0  1 E3 |
+    #  | 0  0  0  1 |       | 0  0  0  1 |
+
+    t = np.array([[], [], []])
+
+
+def edges_to_camera(edges: Vec3DList, camera: Coord3D, eye: Point3D):
+    new_edges = Vec3DList(sep=edges.sep())
+    for edge in edges:
+        p1 = point_to_camera(edge[0], camera, eye)
+        p2 = point_to_camera(edge[1], camera, eye)
+        new_edges.append((p1, p2))
+
+    return new_edges
--- math_utils.py
+++ math_utils.py
@@ -11,13 +11,13 @@ def project(p: Point3D, n: Point3D) -> Point3D:
     a = p[0] * n[0] + p[1] * n[1] + p[2] * n[2]
     b = n[0] * n[0] + n[1] * n[1] + n[2] * n[2]
     res = a / b
-    return n[0] * res, n[1] * res, n[0] * res
+    return n[0] * res, n[1] * res, n[2] * res
 
 
 def normalize(p: Point3D) -> Point3D:
     res = p[0] * p[0] + p[1] * p[1] + p[2] * p[2]
     res = math.sqrt(res)
-    return p[0] / res, p[1] / res, p[0] / res
+    return p[0] / res, p[1] / res, p[2] / res
 
 
 def vetorial_product(u: Point3D, n: Point3D) -> Point3D:
@@ -25,7 +25,3 @@ def vetorial_product(u: Point3D, n: Point3D) -> Point3D:
     j = u[2] * n[0] - u[0] * n[2]
     k = u[0] * n[1] - u[1] * n[0]
     return i, j, k
-
-
-def scale(edges: ):
-    pass
